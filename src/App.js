b71a49c56b5ba6d61051ab3b27775a44ebb63191
Adarshmaurya7 <adarshmaurya777333@gmail.com>
2023-09-30 15:41:12 +0530

    ytclone uplaod

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import SearchResult from "./components/SearchResult";
+import Video_details from "./components/Video_details";
+import { AppContext } from "./context/contextAPI";
+
+const App = () => {
+    return (
+        <AppContext>
+            <BrowserRouter>
+                <div className="flex flex-col h-full">
+                    <Header />
+                    <Routes>
+                        <Route path="/" exact element={<Feed />} />
+                        <Route
+                            path="/searchResult/:searchQuery"
+                            element={<SearchResult />}
+                        />
+                        <Route path="/video/:id" element={<Video_details />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </AppContext>
+    );
+};
+
+export default App;
+
